a00b1835bea39da13583d4811430dff422fc866e
manid2 <maniyoung.d2@gmail.com>
2018-08-11 00:48:41 +0530

    added dataset tarball

--- inc/baserecognizer.h
+++ inc/baserecognizer.h
@@ -14,7 +14,7 @@
 #define FFR_INC_BASERECOGNIZER_H_
 
 #include "opencv2/core/core.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/contrib/contrib.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 #include <logger.h>
